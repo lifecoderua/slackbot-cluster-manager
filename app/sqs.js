808eef6a732b32de34e1d1bbd1f929d92d80d05f
lifecoder <lifecoder.ua@gmail.com>
2019-05-17 17:28:15 +0300

    bi-directional messaging

--- app/sqs.js
+++ app/sqs.js
@@ -7,9 +7,17 @@ var AWS = require('aws-sdk');
 // Create an SQS service object
 const app = Consumer.create({
   queueUrl: process.env.BOT_OUTPUT_QUEUE,
-  handleMessage: async (message) => {
-    console.log(message);
-
+  handleMessage: async (messageJSON) => {
+    console.log('11111', messageJSON);
+    try {
+      const message = JSON.parse(messageJSON.Body);
+      console.log(message);
+  
+      reply(message);
+    } catch(e) {
+      console.log('WHOOPS >>>', e)
+    }
+    
     // Skip message removal
     // throw('NotMyMessage');
   }
@@ -25,36 +33,57 @@ app.on('processing_error', (err) => {
 
 app.start();
 
-
 // Create an SQS service object
 var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
 var params = {
   // DelaySeconds: 10,
-  MessageAttributes: {
-    "Title": {
-      DataType: "String",
-      StringValue: "The Whistler"
-    },
-    "Author": {
-      DataType: "String",
-      StringValue: "John Grisham"
-    },
-    "WeeksOn": {
-      DataType: "Number",
-      StringValue: "6"
-    }
-  },
+  // MessageAttributes: {
+  //   "Title": {
+  //     DataType: "String",
+  //     StringValue: "The Whistler"
+  //   },
+  //   "Author": {
+  //     DataType: "String",
+  //     StringValue: "John Grisham"
+  //   },
+  //   "WeeksOn": {
+  //     DataType: "Number",
+  //     StringValue: "6"
+  //   }
+  // },
   MessageGroupId: 'default',
   MessageDeduplicationId: '1',
   MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
   QueueUrl: process.env.BOT_INPUT_QUEUE,
 };
 
-sqs.sendMessage(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.MessageId);
+// sqs.sendMessage(params, function(err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log("Success", data.MessageId);
+//   }
+// });
+
+
+function reply(message) {
+  console.log('zzz');
+  const response = {
+    trigger_id: message.trigger_id,
+    blocks: 'yay! 123456',
   }
-});
+  
+  params.MessageDeduplicationId = Math.random().toString(),
+  params.MessageBody = JSON.stringify(response);
+
+  console.log('eee');
+
+  sqs.sendMessage(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success+", data.MessageId);
+    }
+  });
+}
